d74252de0bdf92408a7099ccf1305fb2b4a040d1
Bilal Özlü <bilal5071@hotmail.com>
2020-04-10 02:46:58 +0300

    invention page finsihed

--- app.js
+++ app.js
@@ -41,21 +41,24 @@ app.get('/', async function (request, response) {
 });
 
 app.delete('/users/:id', function (request, response) {
-    inventions.updateMany({user_id: request.params.id}, {$set: {hide: true}});
-    users.deleteOne({_id: new mongodb.ObjectId(request.params.id)}, function (error, result) {
-        try {
-            response.redirect('/');
-        }
-        catch {
+    inventions.updateMany({user_id: request.params.id}, {$set: {hide: true}}, function (error, result) {
+        if(error) {
             return response.status(500).send(error);
         }
     });
+    users.deleteOne({_id: new mongodb.ObjectId(request.params.id)}, function (error, result) {
+        if (error) {
+            return response.status(500).send(error);
+        }
+        else {
+            response.redirect('/');
+        }
+    });
 });
-//'photoURL': '', 'materialsUsed': [], 'cost': 0,
+
 app.post('/inventions/exhibit/:user_id/:user_name', (request, response) => {
     inventions.insertOne(Object.assign(request.body, { 'numberOfRates': 0, 'sumOfRates': 0,'rates': [],
-        'user_id': request.params.user_id, 'user_name': request.params.user_name, hide: false}),
-     (error, result) => {
+    'user_id': request.params.user_id, 'user_name': request.params.user_name, hide: false}), (error, result) => {
         try {
             response.redirect('/users/' + request.params.user_id);
         }
@@ -65,20 +68,23 @@ app.post('/inventions/exhibit/:user_id/:user_name', (request, response) => {
     });
 });
 
-// app.get('/inventions', async function (request, response) {
-//     inventions.find({}).toArray((error, result) => {
-//         try {
-//             response.send(result);
-//         }
-//         catch {
-//             return response.status(500).send(error);
-//         }
-//     });
-// });
+app.get('/inventions/:id', async function (request, response) {
+    inventions.find({_id: new mongodb.ObjectId(request.params.id)}).toArray((error, result) => {
+        try {
+            response.render('', {theInvention: result[0], page : 'inventionDetails'});
+        }
+        catch {
+            return response.status(500).send(error);
+        }
+    });
+});
 
 app.get('/users/:id', async function (request, response) {
     users.find({_id: new mongodb.ObjectId(request.params.id)}).toArray((error,result) => {
-        try {
+        if (error) {
+            return response.status(500).send(error);
+        }
+        else {
             inventions.find({hide: false}).toArray((error2, result2) => {
                 try {
                     response.render('', {userInfo: result[0], userInventions: result2, page : 'inventions'});
@@ -87,9 +93,6 @@ app.get('/users/:id', async function (request, response) {
                     return response.status(500).send(error2);
                 }
             });
-        }
-        catch {
-            return response.status(500).send(error);
         }
     });
 });
@@ -113,26 +116,34 @@ app.put('/inventions/rate/:rater_id/:user_id/:id/:oldRate', (request, response) 
     }
     if (addNew == 0) {
         inventions.updateOne({_id: new mongodb.ObjectId(request.params.id), "rates.rater_id": request.params.rater_id},
-            {$inc: {"rates.$.rate": addRate}})
-    }
-    else {
-        inventions.updateOne(
-            {_id: new mongodb.ObjectId(request.params.id)},
-            {$push:  {rates: {rater_id: request.params.rater_id, rate: addRate}}})
-    }
-
-    users.updateOne({_id: new mongodb.ObjectId(request.params.user_id)},
-        {$inc: { sumOfRates: addRate, numberOfRates: addNew}})
-
-    inventions.updateOne(
-        {_id: new mongodb.ObjectId(request.params.id)},
-        {$inc: { sumOfRates: addRate, numberOfRates: addNew }}, function(error,result) {
-            try {
-                response.redirect('/users/' + request.params.rater_id);
-            }
-            catch {
+        {$inc: {"rates.$.rate": addRate}}, function(error,result) {
+            if (error) {
                 return response.status(500).send(error);
             }
+        });
+    }
+    else {
+        inventions.updateOne({_id: new mongodb.ObjectId(request.params.id)},
+        {$push:  {rates: {rater_id: request.params.rater_id, rate: addRate}}}, function(error,result) {
+            if (error) {
+                return response.status(500).send(error);
+            }
+        });
+    }
+    users.updateOne({_id: new mongodb.ObjectId(request.params.user_id)},
+    {$inc: { sumOfRates: addRate, numberOfRates: addNew}}, function(error,result) {
+        if (error) {
+            return response.status(500).send(error);
+        }
+    });
+    inventions.updateOne({_id: new mongodb.ObjectId(request.params.id)},
+    {$inc: { sumOfRates: addRate, numberOfRates: addNew }}, function(error,result) {
+        try {
+            response.redirect('/users/' + request.params.rater_id);
+        }
+        catch {
+            return response.status(500).send(error);
+        }
     });
 });
 
