ff858cc92e3e62a5c5cf1d07e05ad1d0ff0579cb
bilalozlu <bilal.ozlu@outlook.com>
2023-02-19 02:57:37 +0300

    photo directory changed

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 8080;
 const methodOverride = require('method-override')
 
 app.use(methodOverride('_method'));
+app.use('/public',express.static(__dirname +"/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('views', '');
